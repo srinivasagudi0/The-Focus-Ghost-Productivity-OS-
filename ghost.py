774f0f6ad4d750659aea4b3f77dd71de4692a1c2
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-05-05 17:04:26 -0500

    Enhance ghost.py with exit messages and app checks
    Checks if any blacklisted apps are open and just proints them out and I wish this to be one of of my top 3 i will add hints ans dmke it veryvery entertaining.

--- ghost.py
+++ ghost.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 def load_config():
     with open('config.json', 'r') as f:
@@ -9,3 +10,32 @@ config = load_config()
 
 blacklist = config["blacklist"]
 interval = config["interval"]
+exit_message = config["exit"]
+
+import psutil
+import time
+
+def check_apps(blacklist):
+    for proc in psutil.process_iter(["name"]):
+        try:
+            app_name = proc.info["name"]
+            if app_name is None:
+                continue
+            lower_name = app_name.lower()
+
+            for blocked in blacklist:
+                if blocked.lower() in lower_name:
+                    print(f"Haunt 👻: {app_name}")
+        except:
+            pass
+
+# infinite loop
+while True:
+    try:
+        check_apps(blacklist)
+        time.sleep(interval) 
+    except KeyboardInterrupt:
+        print("\n")
+        num = random.randint(1, 25)
+        print(exit_message[num])        
+        break
